acfab5b5fc3668dbe0483931f9ef0667e19bc30d
luuh3k <lucas.digital18@gmail.com>
2022-07-25 14:04:57 -0300

    first commit

---
+++ src/app/components/bookstore-app/product-list/product-item/product-item.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { book } from '../model/Book';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.css']
+})
+export class ProductItemComponent implements OnInit {
+
+
+  @Input
+  livro!: book;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/bookstore-app/product-list/product-list.component.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { book } from "./model/Book";
+
+
+@Injectable()
+
+export class BookService
+{
+    private url = 'https://localhost:44382/api/bookstore'
+
+    httpOptions = { 
+        Headers: new HttpHeaders ({'content-type':'application/json'})
+    }
+
+    constructor (private http: HttpClient){}
+
+
+    getBook(){
+
+        return this.http.get(this.url)
+    }
+
+
+}
